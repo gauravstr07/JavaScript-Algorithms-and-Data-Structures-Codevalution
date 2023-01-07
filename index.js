5b2c0314955961756ec90da650e606eb46abe582
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2023-01-07 15:41:30 +0530

    JavaScript Algorithms - 7 - Fibonacci Sequence

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Welcome to Data-Structure And Algorithms");
+function fibonacci(n) {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return "Fibonacci  --> " + fib;
+}
+
+console.log(fibonacci(5));
+console.log(fibonacci(8));
+console.log(fibonacci(9));
+
+/** output
+ * 
+ * Fibonacci  --> 0,1,1,2,3
+   Fibonacci  --> 0,1,1,2,3,5,8,13
+   Fibonacci  --> 0,1,1,2,3,5,8,13,21
+ * 
+ */
